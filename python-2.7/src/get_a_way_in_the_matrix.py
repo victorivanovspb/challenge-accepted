bf37318f875406f3b1762a47cc24d3b0837143b6
Victor Ivanov <victorivanov.spb@gmail.com>
2017-02-22 16:22:33 +0400

    T. №4 done

--- python-2.7/src/get_a_way_in_the_matrix.py
+++ python-2.7/src/get_a_way_in_the_matrix.py
@@ -68,6 +68,8 @@ class Matrix2D(object):
     def __iter__(self):
         self.curr_x = 0
         self.curr_y = 0
+        self.elem_x = 0
+        self.elem_y = 0
         return self
     
     def next(self):
@@ -75,6 +77,8 @@ class Matrix2D(object):
             raise StopIteration
         
         elem = self.matrix[self.curr_x][self.curr_y]
+        self.elem_x = self.curr_x
+        self.elem_y = self.curr_y
         
         self.curr_x = self.curr_x + 1
         if self.curr_x >= self.width():
@@ -83,19 +87,60 @@ class Matrix2D(object):
 
         return elem
 
+    def get_current_x(self):
+        return self.elem_x
+    
+    def get_current_y(self):
+        return self.elem_y
 
+    def print_matrix(self):
+        for j in range(0,self.height()):
+            for i in range(0, self.width()):
+                print self.matrix[i][j],
+            print
+    
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-    n = 10
-    m = 5
+    n = 10 # matrix width
+    m = 10  # matrix height
     matrix = Matrix2D(n, m)
     rand_list = RandomList(1, n * m)
 
+    # Запись в матрицу элементов из списка со случайными числами.
     for elem, rand_val in itertools.izip(matrix, rand_list):
         elem.value = rand_val
-        
+   
+    # Подсчёт сумм в каждом элементе (с учётом соседних элементов слева и сверху). 
     for elem in matrix:
-        print "%d %d" % (elem.value, elem.sum)
-        
+        x = matrix.get_current_x()
+        y = matrix.get_current_y()
+        prev_sum = list()
+        if x >= 1:
+            prev_sum.append(matrix.matrix[x - 1][y].sum)
+        if y >= 1:
+            prev_sum.append(matrix.matrix[x][y - 1].sum)
+        matrix.matrix[x][y].sum = matrix.matrix[x][y].value + (min(prev_sum) if len(prev_sum) > 0 else 0)
     
+    # Поиск пути. Начиная с последнего элемента просматриваются элементы слева и сверху: тот, у которого своя
+    # сумма меньше (точнее, своя сумма совпадает с суммой текущего элемента минус значение текущего элемента),
+    # становится текущим элементом и записывается в список way.
+    x = matrix.width() - 1
+    y = matrix.height() - 1
+    way = list()
+    way.append((x, y))
+    while x > 0 or y > 0:
+        prev_sum = matrix.matrix[x][y].sum - matrix.matrix[x][y].value
+        if x > 0:
+            if prev_sum == matrix.matrix[x - 1][y].sum:
+                way.append((x - 1, y))
+                x = x - 1
+                continue
+        if y > 0:
+            if prev_sum == matrix.matrix[x][y - 1].sum:
+                way.append((x, y - 1))
+                y = y - 1
+                continue
     
+    # Результат
+    print list(reversed(way))
+    # matrix.print_matrix()
