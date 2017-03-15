7e2affbd2314615be915516ad6a2d6c34fd86124
Victor Ivanov <victorivanov.spb@gmail.com>
2017-03-15 18:45:03 +0400

    UPD T. misc / problem 2 (compress string aabccc)
    Add decorator: compress_and_get_min

--- resp_misc/compress_string_aabccc.py
+++ resp_misc/compress_string_aabccc.py
@@ -7,30 +7,40 @@
         вернуть исходную строку.
 """
 
-def compress(source_str):
-    n_str = ""
+def compress_and_get_min(fn):
+    def wrapped(source):
+        result = fn(source)
+        if len(result) > len(source):
+            return source
+        return result
+    return wrapped
+
+
+@compress_and_get_min
+def compress(source):
+    result = ""
     counter = 0
-    glob_counter = 0
-    curr = ""
-    for ch in source_str:
-        if curr == "":
-            curr = ch
+    actual_ch = ""
+    for idx, ch in enumerate(source):
+        if actual_ch == "":
+            actual_ch = ch
+            counter = 1
+        elif actual_ch == ch:
             counter += 1
-        elif curr == ch:
-            counter += 1
-        elif curr != ch:
-            n_str += curr + str(counter)
-            curr = ch
+        elif actual_ch != ch:
+            result += actual_ch + str(counter)
+            actual_ch = ch
             counter = 1
         
-        glob_counter += 1
-        if glob_counter == len(source_str):
-            n_str += curr + str(counter)
-            
-    return n_str
+        if idx == len(source) - 1:
+            result += actual_ch + str(counter)
+        
+    return result
+
 
 def formatter(label, str1, str2, sh = "  "):
-    return label + ":\n" + sh + str1 + "\n" + sh + str2 + "\n"
+    return label + ":\n" + sh + "[" + str1 + "]\n" + sh + "[" + str2 + "]\n"
+
 
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
@@ -47,6 +57,7 @@ if __name__ == "__main__":
     s4 = ".................................."
     print formatter("s4", s4, compress(s4))
 
-
+    s5 = ""
+    print formatter("s5", s5, compress(s5))
 
 
